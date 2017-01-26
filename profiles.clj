160349f1722f5657eefa0174935c0d83c188ef69
Oren Dobzinski <oren@desk.com>
2017-01-25 22:57:42 -0500

    add profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[lein-try "0.4.3"]
+                  [lein-ancient "0.5.5"]
+                  [cider/cider-nrepl "0.12.0"]
+                  [lein-exec "0.3.1"]
+                  [refactor-nrepl "2.2.0"]]}}
