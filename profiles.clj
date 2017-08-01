33a4f524e08c6fe52f7a73e5e5a4fa3b2dccd0b4
Oren Dobzinski <oren@desk.com>
2017-08-01 15:40:16 -0400

    update profiles file

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[lein-try "0.4.3"]
-                  [lein-ancient "0.5.5"]
-                  [cider/cider-nrepl "0.12.0"]
-                  [lein-exec "0.3.1"]
-                  [refactor-nrepl "2.2.0"]]}}
+                  [lein-ancient "0.6.10"]
+                  [cider/cider-nrepl "0.14.0"]
+                  [lein-exec "0.3.6"]
+                  [refactor-nrepl "2.3.1"]]}}
